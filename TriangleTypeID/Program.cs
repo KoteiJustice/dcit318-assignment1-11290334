2487db0562ea9a3f2897de411d80fe64029fa840
Yung-Phantom <yungphantom019@gmail.com>
2025-08-01 10:09:17 +0000

    Triangle type identifier

---
+++ TriangleTypeID/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class TriangleTypeID
+{
+    static void Main()
+    {
+        double sideA, sideB, sideC;
+        while (true)
+        {
+            Console.WriteLine("Please input the sizes of each side of your triangle \nSide A:");
+            string inputA = Console.ReadLine();
+            Console.WriteLine("Side B:");
+            string inputB = Console.ReadLine();
+            Console.WriteLine("Side C:");
+            string inputC = Console.ReadLine();
+            if (double.TryParse(inputA, out sideA) && double.TryParse(inputB, out sideB) && double.TryParse(inputC, out sideC))
+            {
+                if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+                {
+                    if (sideA == sideB && sideA == sideC)
+                    {
+                        Console.WriteLine("Equilateral");
+                    }
+                    else if (sideA == sideB || sideA == sideC || sideB == sideC)
+                    {
+                        Console.WriteLine("Isosceles");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Scalene");
+                    }
+                    break;
+                }
+            }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter valid numbers.");
+                }
+        }
+    }
+}
